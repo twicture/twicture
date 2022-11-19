cb523f1a4f92eb59a7146c34bfe7fff2b95ac473
J0sueTM <teodoro.josue@protonmail.ch>
2022-11-19 17:40:37 -0300

    moved backend to root

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: backend
+web: go run .
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := router.Run(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
